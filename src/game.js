bcdbe14bf0c0dbc8513c2fc1a2daf064d54fb019
typable <typable.dev@gmail.com>
2021-04-21 12:15:45 +0200

    Added quit function & try-catch block

--- src/game.js
+++ src/game.js
@@ -7,6 +7,7 @@ export default class Game {
 		this.height = height || 600;
 		this.fps = 1000 / (fps || 60);
 		this.then = Date.now();
+		this.running = false;
 		this.canvas = document.createElement('canvas');
 		scaleCanvas(this.canvas, this.width, this.height);
 		this.g = this.canvas.getContext('2d');
@@ -19,27 +20,42 @@ export default class Game {
 		for(const sprite of this.sprites) {
 			await sprite.load();
 		}
-		if(this.onspriteload) {
-			this.onspriteload();
+		this.running = true;
+		if(this.oninit) {
+			this.oninit();
 		}
 		requestAnimationFrame(this.loop.bind(this));
 		this.update();
 		this.render(this.g);
 	}
+	quit() {
+		this.running = false;
+		if(this.onquit) {
+			this.onquit();
+		}
+	}
 	loop() {
-		requestAnimationFrame(this.loop.bind(this));
-		this.now = Date.now();
-		this.elapsed = this.now - this.then;
-		if(this.elapsed > this.fps) {
-			this.then = this.now - (this.elapsed % this.fps);
-			for(const event of this.events) {
-				if(this['on' + event.type]) {
-					this['on' + event.type](event);
+		try {
+			if(this.running) {
+				requestAnimationFrame(this.loop.bind(this));
+				this.now = Date.now();
+				this.elapsed = this.now - this.then;
+				if(this.elapsed > this.fps) {
+					this.then = this.now - (this.elapsed % this.fps);
+					for(const event of this.events) {
+						if(this['on' + event.type]) {
+							this['on' + event.type](event);
+						}
+					}
+					this.update();
+					this.render(this.g);
+					this.events = [];
 				}
 			}
-			this.update();
-			this.render(this.g);
-			this.events = [];
+		}
+		catch(error) {
+			console.error(error);
+			this.running = false;
 		}
 	}
 	update() {
