41c2f5a79953ddc4f03a0ca42a2c840517a6a799
typable <typable.dev@gmail.com>
2021-04-22 13:42:43 +0200

    Updated spritesheet dimension

--- src/sprite/spritesheet.js
+++ src/sprite/spritesheet.js
@@ -2,27 +2,28 @@ import Sprite from './sprite.js';
 import { scaleCanvas } from '../util/canvas.js';
 
 export default class Spritesheet extends Sprite {
-	constructor(path, size) {
+	constructor(path, width, height) {
 		super(path);
-		this.size = size;
+		this.width = width;
+		this.height = height || width;
 		this.sprites = [];
 	}
 	async load() {
 		await super.load();
 		const {width, height} = this.image;
-		if(width % this.size !== 0 || height % this.size !== 0) {
-			throw new Error(`Invalid size ${this.size} for spritesheet '${this.path}'!`);
+		if(width % this.width !== 0 || height % this.height !== 0) {
+			throw new Error(`Invalid dimension ${this.width}x${this.height} for spritesheet '${this.path}'!`);
 		}
-		this.sprites = new Array(parseInt(width / this.size) * parseInt(height / this.size));
+		this.sprites = new Array(parseInt(width / this.width) * parseInt(height / this.height));
 		const canvas = document.createElement('canvas');
-		scaleCanvas(canvas, this.size, this.size);
+		scaleCanvas(canvas, this.width, this.height);
 		const g = canvas.getContext('2d');
 		g.imageSmoothingEnabled = false;
 		for(let i = 0; i < this.sprites.length; i++) {
-			const y = parseInt(i / (width / this.size));
-			const x = parseInt(i % (width / this.size));
-			g.clearRect(0, 0, this.size, this.size);
-			g.drawImage(this.image, -x * this.size, -y * this.size, width, height);
+			const y = parseInt(i / (width / this.width));
+			const x = parseInt(i % (width / this.width));
+			g.clearRect(0, 0, this.width, this.height);
+			g.drawImage(this.image, -x * this.width, -y * this.height, width, height);
 			const sprite = new Sprite(canvas.toDataURL('image/png'));
 			await sprite.load();
 			this.sprites[i] = sprite;
